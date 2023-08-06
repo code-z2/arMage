1e774dff73950f5a8a8e5ab6921407204ec1ad8c
peteruche21 <anyaogupeter601@gmail.com>
2023-08-06 14:24:24 +0100

    ui: bootstrap nextjs

--- packages/edge/src/constants.ts
+++ packages/edge/src/constants.ts
@@ -5,7 +5,7 @@ export const PORT = process.env.PORT || 3000;
 export const HOST = process.env.HOST || '127.0.0.1';
 export const BOOTSTRAP = process.env.BOOTSTRAP || 'ws://127.0.0.1:3001';
 export const GOSSIP_INTERVAL = parseInt(process.env.GOSSIP_INTERVAL || `${60000 * 30}`); // 30 minutes
-export const EDGE_ID = createHash('sha256').update(`${HOST}:${PORT}`).digest('hex');
+export const EDGE_ID = '0x' + createHash('sha256').update(`${HOST}:${PORT}`).digest('hex');
 export const SELF = (timestamp: number, location: [number, number]): EdgeNode => {
   return {
     id: EDGE_ID,
